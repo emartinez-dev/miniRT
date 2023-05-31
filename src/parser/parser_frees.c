dbd6c5d8f60a058a6e8d3dddcfa863d79954b99e
emartinez <enri.fc.98@gmail.com>
2023-05-31 19:46:09 +0200

    feat(parser): create functions to clear the scene and its new lists

---
+++ src/parser/parser_frees.c
@@ -0,0 +1,17 @@
+#include "parser.h"
+
+void	free_objects(void *content)
+{
+	t_object	*obj;
+
+	obj = content;
+	free(obj->ptr);
+	free(obj);
+}
+
+void	free_scene(t_scene *scene)
+{
+	free_matrix(scene->scene_str);
+	ft_lstclear(&scene->lights, free_objects);
+	ft_lstclear(&scene->objects, free_objects);
+}
