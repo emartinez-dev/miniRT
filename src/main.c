6bee89f8d9b7496b3462ff15bb88045aa01336e0
emartinez <enri.fc.98@gmail.com>
2023-05-31 20:37:48 +0200

    bugfix(parser): I mixed two error handling implementations

--- src/main.c
+++ src/main.c
@@ -9,21 +9,12 @@ void	ft_leaks(void)
 int	main(int argc, char **argv)
 {
 	t_scene	scene;
-	t_list	*list;
-	t_object	*ptr;
-	t_amb_light	*light;
 
 	atexit(ft_leaks);
-	if (params_error(argc, argv, &scene) < 0)
+	if (params_error(argc, argv, &scene))
 		return (1);
 	read_scene(&scene);
 	extract_objects(&scene);
-	printf("scene lights main: %p\n", scene.lights);
-	list = scene.lights;
-	ptr = (t_object *)list->content;
-	light = (t_amb_light *)ptr->ptr;
-	ft_printf("ptr id: %d\n", ptr->type);
-	ft_printf("light r: %d\n", light->c.r);
 	free_scene(&scene);
 	return (0);
 }
