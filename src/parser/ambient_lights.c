0642e850163d84efc20edd673a0632e6501b49ca
emartinez <enri.fc.98@gmail.com>
2023-05-31 20:09:39 +0200

    feat(errors): add error handler for ambient lights

--- src/parser/ambient_lights.c
+++ src/parser/ambient_lights.c
@@ -20,6 +20,30 @@ t_object	*parse_obj_ambient_light(char *line)
 	}
 	else
 		obj->error = 1;
+	obj->error = errors_ambient_light(light, obj);
 	free_matrix(split);
 	return (obj);
+}
+
+/* only check for range errors, the other errors were handled with the matrix
+split length */
+
+int	errors_ambient_light(t_amb_light *light, t_object *obj)
+{
+	int	error;
+
+	if (obj->error == 1)
+	{
+		ft_putstr_fd(ERR_MISSING_PARAMS, STDERR_FILENO);
+		return (1);
+	}
+	error = 0;
+	if (light->ratio < 0.0 || light->ratio > 1.0)
+		error = 2;
+	if (light->c.r > 255 || light->c.g > 255 || light->c.b > 255 || \
+			light->c.r < 0 || light->c.g < 0 || light->c.b < 0)
+		error = 2;
+	if (error == 2)
+		ft_putstr_fd(ERR_RANGE, STDERR_FILENO);
+	return (error);
 }
