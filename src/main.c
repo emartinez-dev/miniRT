54d0cbd13025b52957a4dc2feb64c212a2cae4f3
emartinez <enri.fc.98@gmail.com>
2023-05-31 15:46:20 +0200

    feat(parser): implement functions to read scene file and to free the scene

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include "parser.h"
 
+void	ft_leaks(void)
+{
+	system("leaks miniRT");
+}
+
 int	main(int argc, char **argv)
 {
 	t_scene	scene;
 
+	atexit(ft_leaks);
 	if (params_error(argc, argv, &scene) < 0)
 		return (1);
+	read_scene(&scene);
+	free_scene(&scene);
 }
