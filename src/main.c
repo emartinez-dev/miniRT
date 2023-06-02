00eb5a707d7b7574e90f6df664d31e5258849482
emartinez <enri.fc.98@gmail.com>
2023-06-02 17:46:35 +0200

    feat(engine): init mlx

--- src/main.c
+++ src/main.c
@@ -1,23 +1,45 @@
 #include "parser.h"
+#include "engine.h"
+#include "MLX42.h"
 #include <stdio.h>
+#include <limits.h>
+#include "vec3.h"
 
 void	ft_leaks(void)
 {
 	system("leaks -q miniRT");
 }
 
+void render(mlx_t *mlx, mlx_image_t *img)
+{
+	for (uint32_t h = 0; h < img->height; h++)
+		for (uint32_t w = 0; w < img->width; w++)
+				mlx_put_pixel(img, w, h, rgb_to_hex((t_color){0, 0, 0}));
+	mlx_image_to_window(mlx, img, 0, 0);
+}
+
 int	main(int argc, char **argv)
 {
-	t_scene	scene;
+	t_scene		scene;
+	mlx_t		*mlx;
+	mlx_image_t	*img;
 
 	atexit(ft_leaks);
+	mlx = mlx_init(WIDTH, HEIGHT, "miniRT", true);
+	if (!mlx)
+		return (1);
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	if (params_error(argc, argv, &scene))
 		return (1);
 	read_scene(&scene);
 	if (extract_objects(&scene) != 0)
 		ft_printf("\nThe provided scene: %s is bad formed\n\n", argv[1]);
 	else
+	{
 		print_todo(&scene);
+		render(mlx, img);
+	}
+	mlx_loop(mlx);
 	free_scene(&scene);
 	return (0);
 }
