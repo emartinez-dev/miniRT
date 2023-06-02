65c9988af4f3a2b26dd97d17a036bcdf6dff9784
emartinez <enri.fc.98@gmail.com>
2023-06-02 16:39:55 +0200

    bugfix(parser): fix leak after removing camera from objects

--- src/parser/parser_frees.c
+++ src/parser/parser_frees.c
@@ -15,4 +15,5 @@ void	free_scene(t_scene *scene)
 	free_matrix(scene->scene_str);
 	ft_lstclear(&scene->lights, free_objects);
 	ft_lstclear(&scene->objects, free_objects);
+	free(scene->camera);
 }
