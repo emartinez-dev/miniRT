bc6328e1e8a721e65199c5ed6e26a356353c30e0
emartinez <enri.fc.98@gmail.com>
2023-06-22 12:53:32 +0200

    feat(colors): add function to clamp color values to the max

--- src/engine/colors.c
+++ src/engine/colors.c
@@ -11,3 +11,14 @@ unsigned int	rgb_to_hex(t_color color)
 	result |= 0x000000FF;
 	return (result);
 }
+
+t_color	trim_colors(t_color	color)
+{
+	if (color.r > 255)
+		color.r = 255;
+	if (color.g > 255)
+		color.g = 255;
+	if (color.b > 255)
+		color.b = 255;
+	return (color);
+}
