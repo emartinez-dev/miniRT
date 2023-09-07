1f831191c3891a8d4dd9fd170a52d79fcf88127c
[Juan A.] <juan-aga@student.42.malaga.com>
2023-09-07 12:45:33 +0200

    ADD: intersect_plane function.

--- include/engine.h
+++ include/engine.h
@@ -91,6 +91,7 @@ t_hit			hit_objects(t_ray *ray, t_list *objects);
 t_hit			hit_sphere(t_sphere *sp, t_ray ray, t_hit hit);
 t_hit			hit_plane(t_plane *pl, t_ray ray, t_hit hit);
 t_hit			hit_cylinder(t_cylinder *cyl, t_ray ray, t_hit hit);
+double			intersect_plane(t_ray ray, t_v3 norm, t_v3 p);
 
 /*	phong.c	*/
 t_color			phong_light(t_scene *scene, t_hit *hit);
--- src/objects/o_plane.c
+++ src/objects/o_plane.c
@@ -6,7 +6,7 @@
 t_hit	hit_plane(t_plane *pl, t_ray ray, t_hit hit)
 {
 	t_hit	temp;
-	double	norm_dist;
+/*	double	norm_dist;
 	double	norm_ray;
 	t_v3	oc;
 
@@ -17,6 +17,8 @@ t_hit	hit_plane(t_plane *pl, t_ray ray, t_hit hit)
 	oc = vec3_sub(pl->p, ray.origin);
 	norm_dist = vec3_dot(oc, pl->norm);
 	temp.t = norm_dist / norm_ray;
+*/	temp.t = intersect_plane(ray, pl->norm, pl->p);
+	temp.view = ray.direction;
 	if ((hit.t > temp.t || hit.t == -1.0) && temp.t > EPSILON)
 	{
 		temp.color = pl->c;
@@ -25,4 +27,19 @@ t_hit	hit_plane(t_plane *pl, t_ray ray, t_hit hit)
 		hit = temp;
 	}
 	return (hit);
+}
+
+double	intersect_plane(t_ray ray, t_v3 norm, t_v3 p)
+{
+	double	norm_dist;
+	double	norm_ray;
+	t_v3	oc;
+
+	norm_ray = vec3_dot(ray.direction, norm);
+	if (norm_ray >= 0)
+		return (-1.0);
+	oc = vec3_sub(p, ray.origin);
+	norm_dist = vec3_dot(oc, norm);
+	return ((double)(norm_dist / norm_ray));
+
 }
